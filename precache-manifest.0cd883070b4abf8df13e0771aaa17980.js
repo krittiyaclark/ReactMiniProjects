8eef8f282cdf27ceb86a706f0b79a9cfc65f0bba
krittiyaclark <krittiyaclark@gmail.com>
2020-02-18 11:29:44 -0500

    Updates

---
+++ precache-manifest.0cd883070b4abf8df13e0771aaa17980.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ae9d008460ba6bd52a69b13bc9ffb76",
+    "url": "/krittiyaclark/ReactMiniProjects/index.html"
+  },
+  {
+    "revision": "035130d1141e404166d0",
+    "url": "/krittiyaclark/ReactMiniProjects/static/css/main.2c818765.chunk.css"
+  },
+  {
+    "revision": "494929a787a1be3a7979",
+    "url": "/krittiyaclark/ReactMiniProjects/static/js/2.c793f56a.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/krittiyaclark/ReactMiniProjects/static/js/2.c793f56a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "035130d1141e404166d0",
+    "url": "/krittiyaclark/ReactMiniProjects/static/js/main.4283c446.chunk.js"
+  },
+  {
+    "revision": "b31b2e5cc035a3843f9d",
+    "url": "/krittiyaclark/ReactMiniProjects/static/js/runtime-main.7f1ae876.js"
+  }
+]);
