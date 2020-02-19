ba92b4a83d9b042dc028c3a9616ee63ddf549622
krittiyaclark <krittiyaclark@gmail.com>
2020-02-19 18:20:30 -0500

    Changed submodule to directory

---
+++ react-basics/src/App.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+
+import { CardList } from './component/card-list/card-list.component';
+
+import { SearchBox } from './component/search-box/search-box.component';
+
+import './App.css';
+
+class App extends Component {
+	constructor() {
+		super();
+
+		this.state = {
+			monsters: [],
+			searchField: ''
+		};
+	}
+
+	handleChange = e => {
+		this.setState({ searchField: e.target.value });
+	};
+
+	componentDidMount() {
+		fetch('https://jsonplaceholder.typicode.com/users')
+			.then(response => response.json())
+			.then(monster => this.setState({ monsters: monster }));
+	}
+
+	render() {
+		const { monsters, searchField } = this.state;
+		const filteredMonsters = monsters.filter(monster =>
+			monster.name.toLowerCase().includes(searchField.toLowerCase())
+		);
+
+		return (
+			<div className='App'>
+				<h1>Monsters Rolodex</h1>
+				<SearchBox
+					placeholder='search monster'
+					handleChange={this.handleChange}
+				/>
+				<CardList monster={filteredMonsters} />
+			</div>
+		);
+	}
+}
+
+export default App;
---
+++ react-redux-tutorial/src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+class ExampleComponent extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			articles: [
+				{ title: 'React Redux Tutorial for Beginners', id: 1 },
+				{ title: 'TypeScript tutorial for beginners', id: 2 }
+			]
+		};
+	}
+
+	render() {
+		const { articles } = this.state;
+		return (
+			<ul>
+				{articles.map(el => (
+					<li key={el.id}>{el.title}</li>
+				))}
+			</ul>
+		);
+	}
+}
+
+export default ExampleComponent;
