3c904c44522713d05c6d2163f0e9b5946fe4528d
krittiyaclark <krittiyaclark@gmail.com>
2020-07-08 11:29:27 -0400

    Search the cocktail in the SearchForm component

--- cocktails/src/components/SearchForm.js
+++ cocktails/src/components/SearchForm.js
@@ -1,5 +1,35 @@
-import React from 'react'
+import React, { useRef, useEffect } from 'react'
 
-export default function SearchForm() {
-	return <h1>searchform component</h1>
+export default function SearchForm({ setSearchTerm }) {
+	const searchValue = useRef('')
+
+	useEffect(() => {
+		searchValue.current.focus()
+	}, [])
+
+	const handleSubmit = (event) => {
+		event.preventDefault()
+	}
+
+	const searchCocktail = () => {
+		setSearchTerm(searchValue.current.value)
+	}
+
+	return (
+		<section className='section'>
+			<h2 className='section-title'>search cocktails</h2>
+			<form className='form search-form' onSubmit={handleSubmit}>
+				<div className='form-control'>
+					<label htmlFor='name'>search your favorite cocktail</label>
+					<input
+						type='text'
+						name='name'
+						id='name'
+						ref={searchValue}
+						onChange={searchCocktail}
+					/>
+				</div>
+			</form>
+		</section>
+	)
 }
